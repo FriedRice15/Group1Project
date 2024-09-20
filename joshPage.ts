6c32524f79e28595487395ee31d780ac461bb1e8
Joshua Powell <joshpowell015@gmail.com>
2024-09-19 20:48:42 -0600

    the final

--- joshPage.ts
+++ joshPage.ts
@@ -12,11 +12,12 @@ export class HotoffdeGrill extends BasePage {
     emailAdd: By = By.id ('[id="1440555156"]');
     phoneNum: By = By.id ('[id="1154878065"]');
     comment: By = By.id ('[id="1872492295"]');
+    popUp: By = By.xpath ('(//div[@class="dmPopupClose dm-common-icons-close oneIcon"])[1]');
     constructor() {
         super({url:"https://www.hotoffdegrill.com/"});
     };
 
-    async restaurantScreeshot(elementBy: By) {
+    async restaurantScreenshot(elementBy: By) {
     const hover = this.driver.actions();
     const startElement = await this.getElement(elementBy);
     await this.actionWiggle(hover, startElement, 100);
@@ -28,4 +29,34 @@ export class HotoffdeGrill extends BasePage {
                 else console.log
         }
     )};
+    async instaTabs() {
+        let myTabs = await this.driver.getAllWindowHandles();
+        await  this.driver.switchTo().window(myTabs[1]);
+        fs.writeFile(`${__dirname})/this.insta.png`,
+            await this.driver.takeScreenshot(),"base64",
+            (e) => {
+                if(e) console.log(e)
+                    else console.log('da screenshot');
+            });
+            await this.driver.close();
+            await this.driver.switchTo().window(myTabs[0]);
+    };
+    async emailTabs() {
+        let myTabs = await this.driver.getAllWindowHandles();
+        await  this.driver.switchTo().window(myTabs[1]);
+        fs.writeFile(`${__dirname})/email.png`,
+            await this.driver.takeScreenshot(),"base64",
+            (e) => {
+                if(e) console.log(e)
+                    else console.log('da screenshot');
+            });
+            await this.driver.close();
+            await this.driver.switchTo().window(myTabs[0]);  
+        };
+        async scrollToElement(elementBy: By) {
+            const scrollThing = await this.getElement(elementBy);
+            await this.driver.actions()
+            .move({origin: scrollThing})
+            .perform()
+        };
 };
--- joshTest.test.ts
+++ joshTest.test.ts
@@ -3,22 +3,32 @@ const restaurant = new HotoffdeGrill();
 
 test('Instagram Button', async () => {
     await restaurant.navigate();
+    await restaurant.driver.sleep(2000);
+    await restaurant.click (restaurant.popUp);
+    await restaurant.scrollToElement(restaurant.about);
     await restaurant.click(restaurant.about);
+    await restaurant.scrollToElement(restaurant.insta);
     await restaurant.click(restaurant.insta);
+    await restaurant.instaTabs();
 });
 
 test('Email', async () => {
     await restaurant.navigate();
+    await restaurant.driver.sleep(2000);
+    await restaurant.scrollToElement(restaurant.email);
     await restaurant.click(restaurant.email);
+    await restaurant.emailTabs();
 });
 
 test('Send Message', async () => {
     await restaurant.navigate();
+    await restaurant.driver.sleep(2000);
+    await restaurant.scrollToElement(restaurant.firstName);
     await restaurant.setInput(restaurant.firstName, "Josh");
     await restaurant.setInput(restaurant.lastName, "Powell");
     await restaurant.setInput(restaurant.emailAdd, "testemail@gmail.com");
     await restaurant.setInput(restaurant.phoneNum, 1234567890);
     await restaurant.setInput(restaurant.comment, "great food!");
-    await restaurant.restaurantScreeshot(restaurant.sendMessage);
+    await restaurant.restaurantScreenshot(restaurant.sendMessage);
     await restaurant.driver.quit();
 });
